163ebf3cd0dde5951452106fbe5d46d49a18408d
Elvis Dzvimbo <el.tnash@gmail.com>
2024-09-27 11:24:40 +0200

    update.code

---
+++ js/Classes.js
@@ -0,0 +1,35 @@
+class Pizza {
+    //Parent class
+    crust = "original";    //public variable, hardcoded values
+    #sauce = "traditional";  // private variable, hardcoded values
+    #size;
+
+    constructor(parameterPizzaSize){
+        this.#size = parameterPizzaSize; //private variable that takes a parameter as its value
+    }
+
+    //getters and setters
+
+    getCrust(){
+        return this.crust;
+
+    }
+    setCurst(parameterPizzaCrust){
+        this.crust = parameterPizzaCrust;
+    }
+
+    //methods
+
+    hereYouGo(){
+        // displaying our data through the console using values stored in public and private variables
+        console.log(`Here's your ${this.crust} ${this.#sauce} ${this.#size} pizza`);
+    };
+
+
+}
+
+console.log(Pizza.crust)
+
+
+
+  
